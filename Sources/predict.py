83fc548ee5ea77af29eaeb22b99bebafff688b66
lhys4297 <leehys4297@gmail.com>
2023-12-20 20:20:57 +0900

    predict 작성중#2

--- Sources/predict.py
+++ Sources/predict.py
@@ -1,12 +1,18 @@
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.preprocessing import LabelEncoder
 from movie_recommender import *
+#from data_reader import *
 
 # 데이터 전처리
 def prepare_movie_features(movies_df):
     movies_df['genre'] = LabelEncoder().fit_transform(movies_df['genre'])
     movies_df['director'] = LabelEncoder().fit_transform(movies_df['director'])
     return movies_df[['genre', 'director']]
+
+def prepare_user_history(user_history_df, movies_df):
+    movie_features = prepare_movie_features(movies_df)
+    user_history_df = user_history_df.join(movie_features, on='movieId')
+    return user_history_df
 
 def predict_user_rating(user_history_df, movie):
 
